18e36e67e87a0805dd3dd2c7afa3bfa13f3f4ed0
Julian Tallar <jtallar@itba.edu.ar>
2021-04-01 14:41:12 -0300

    Remove L1 distance, only use L2

--- analysis.py
+++ analysis.py
@@ -15,7 +15,6 @@ N = int(sys.stdin.readline()) # Full side length --> NxN or NxNxN
 # Each i list element corresponds to i-th time
 live_count = []
 # TODO: Decidir entre distancia L1 y L2
-furthest_distance_l1 = []
 furthest_distance_l2 = []
 cur_max_distance = 0
 restart = False
@@ -27,17 +26,12 @@ for line in sys.stdin:
         # Particle count in current time
         live_count.append(int(line.rstrip()))
         # Initialize furthest distance to 0
-        furthest_distance_l1.append(0)
         furthest_distance_l2.append(0)
         restart = False
         continue
     
     # Current time, reading alive particle positions
     cell = obj.Cell.new_from_array(line.split())
-    # Update futhest L1 distance
-    cur_distance_l1 = cell.distance_to_origin_l1()
-    if cur_distance_l1 > furthest_distance_l1[-1]:
-        furthest_distance_l1[-1] = cur_distance_l1
     # Update futhest L2 distance
     cur_distance_l2 = cell.distance_to_origin_l2()
     if cur_distance_l2 > furthest_distance_l2[-1]:
@@ -47,7 +41,6 @@ step_count = len(live_count)
 
 if plot_boolean:
     utils.plot_values(range(0, step_count), live_count)           # Plot live_count=f(t)
-    utils.plot_values(range(0, step_count), furthest_distance_l1) # Plot furthest_distance_l1=f(t)
     utils.plot_values(range(0, step_count), furthest_distance_l2) # Plot furthest_distance_l2=f(t)
 
     # Hold execution until all plots are closed
