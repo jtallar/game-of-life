2f55cbab2438081d55b5135f135a903fc10584b0
Julian Tallar <jtallar@itba.edu.ar>
2021-04-02 16:30:01 -0300

    Moved analysis function to separate file

--- analysis.py
+++ analysis.py
@@ -1,6 +1,7 @@
 import sys
 
 import objects as obj
+import analyzer
 import utils
 
 if len(sys.argv) <= 1:
@@ -8,70 +9,4 @@ if len(sys.argv) <= 1:
 else:
     plot_boolean = (sys.argv[1] == 'plot')
 
-def analyze_input(source, plot):
-    # Read dimension and side length
-    dim = int(source.readline())
-    N = int(source.readline()) # Full side length --> NxN or NxNxN
-
-    # Each i list element corresponds to i-th time
-    live_count = []
-    live_count_slope = []
-
-    furthest_distance_l2 = []
-    furthest_distance_l2_slope = []
-    end_by_border = False
-
-    cur_max_distance = 0
-    restart = False
-    for line in source:
-        if "*" == line.rstrip():
-            restart = True
-            continue
-        if restart:
-            # Particle count in current time
-            live_count.append(int(line.rstrip()))
-            live_count_slope.append(utils.regression_slope(live_count))
-            # Initialize furthest distance to 0
-            if furthest_distance_l2:
-                furthest_distance_l2_slope.append(utils.regression_slope(furthest_distance_l2))
-            furthest_distance_l2.append(0)
-            restart = False
-            continue
-        
-        # Current time, reading alive particle positions
-        cell = obj.Cell.new_from_array(line.split())
-        # Update futhest L2 distance
-        cur_distance_l2 = cell.distance_to_origin_l2()
-        if cur_distance_l2 > furthest_distance_l2[-1]:
-            furthest_distance_l2[-1] = cur_distance_l2
-        if cell.is_border_cell(N):
-            end_by_border = True
-    # Add last slope for furthest_distance_l2_slope
-    furthest_distance_l2_slope.append(utils.regression_slope(furthest_distance_l2))
-
-    step_count = len(live_count)
-
-    # Figure out how it ended
-    if end_by_border: end_print = 'Got to edge 🏁'
-    elif live_count[-1] == 0: end_print = 'All cells died 💀️'
-    else: end_print = 'Repetition 🔁'
-
-    observables = obj.Observables(end_print, step_count, live_count_slope[-1], furthest_distance_l2_slope[-1])
-    # Print stats
-    print(observables)
-    # Plot values
-    if plot:
-        utils.init_plotter()
-
-        utils.plot_values(range(0, step_count), 'tiempo', live_count, 'celdas vivas')               # Plot live_count=f(t)
-        utils.plot_values(range(0, step_count), 'tiempo', live_count_slope, 'velocidad celdas vivas')  # Plot live_count_slope=f(t)
-        utils.plot_values(range(0, step_count), 'tiempo', furthest_distance_l2, 'radio de patrón')  # Plot furthest_distance_l2=f(t)
-        utils.plot_values(range(0, step_count), 'tiempo', furthest_distance_l2_slope, 'velocidad radio de patrón')  # Plot furthest_distance_l2_slope=f(t)
-
-        # Hold execution until all plots are closed
-        utils.hold_execution()
-    
-    return observables
-
-
-analyze_input(sys.stdin, plot_boolean)
+analyzer.analyze_input(sys.stdin, plot_boolean)
