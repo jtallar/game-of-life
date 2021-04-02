fb5bc4e545494b040497db50a2f0392e847903c1
Julian Tallar <jtallar@itba.edu.ar>
2021-04-02 11:29:46 -0300

    Observable class

--- objects.py
+++ objects.py
@@ -59,3 +59,17 @@ class Cell(object):
         if any(v == -offset or v == side - 1 - offset for v in self.coordinate_list()):
             return True
         return False
+
+class Observables(object):
+    def __init__(self, end_print, step_count, live_count_slope, max_distance_slope):
+        self.end_print = end_print
+        self.step_count = step_count
+        self.live_count_slope = live_count_slope
+        self.max_distance_slope = max_distance_slope
+    
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        return """Ended by: %s\nTotal number of generations (including initial one): %s
+            \nFinal live_count slope: %s\nFinal futhest_distance slope: %s\n""" % (self.end_print, self.step_count, self.live_count_slope, self.max_distance_slope)
